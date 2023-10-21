d1afceae4c79557a19ac2b25784bc1fb95ae7e08
biplob192 <biplob.net2@gmail.com>
2023-10-21 12:52:29 +0600

    commented the requirment menu from side_bar

--- resources/views/include/master_sidebar.blade.php
+++ resources/views/include/master_sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="/" class="brand-link">
-        <img src="{{asset('/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="{{ asset('/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">MY COMPANY</span>
     </a>
 
@@ -14,7 +14,7 @@
                 <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="{{route('auth.dashboard')}}" class="nav-link">
+                    <a href="{{ route('auth.dashboard') }}" class="nav-link">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Dashboard
@@ -23,7 +23,7 @@
 
                 </li>
                 @role('admin')
-                {{-- <li class="nav-item">
+                    {{-- <li class="nav-item">
                     <a href="{{route('users.index')}}" class="nav-link">
                         <i class="nav-icon fas fa-users"></i>
                         <p>
@@ -31,7 +31,7 @@
                         </p>
                     </a>
                 </li> --}}
-                {{-- <li class="nav-item">
+                    {{-- <li class="nav-item">
                     <a href="{{route('attendance.index')}}" class="nav-link">
                         <i class="nav-icon fas fa-user-clock"></i>
                         <p>
@@ -40,7 +40,7 @@
                     </a>
                 </li> --}}
 
-                <li class="nav-item">
+                    {{-- <li class="nav-item">
                     <a href="requirements" class="nav-link">
                         <i class="nav-icon fas fa-cloud-sun-rain"></i>
                         <p>
@@ -66,93 +66,101 @@
                             </a>
                         </li>
                     </ul>
-                </li>
+                </li> --}}
 
-                <li class="nav-item">
-                    <a href="crops" class="nav-link">
-                        <i class="nav-icon fas fa-seedling"></i>
-                        <p>
-                            Crops
-                            <i class="fas fa-angle-left right"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="{{route('crops.index')}}" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>
-                                    Index
-                                </p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{route('crops.create')}}" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>
-                                    New Crop
-                                </p>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
+                    <li class="nav-item">
+                        <a href="crops" class="nav-link">
+                            <i class="nav-icon fas fa-seedling"></i>
+                            <p>
+                                Crops
+                                <i class="fas fa-angle-left right"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="{{ route('crops.index') }}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>
+                                        Index
+                                    </p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ route('crops.create') }}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>
+                                        New Crop
+                                    </p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ route('crop_requirements.create') }}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>
+                                        New Requirement
+                                    </p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
 
-                <li class="nav-item">
-                    <a href="categories" class="nav-link">
-                        <i class="nav-icon fas fa-layer-group"></i>
-                        <p>
-                            Category
-                            <i class="fas fa-angle-left right"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="{{route('categories.index')}}" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>
-                                    Index
-                                </p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{route('categories.create')}}" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>
-                                    New Category
-                                </p>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
+                    <li class="nav-item">
+                        <a href="categories" class="nav-link">
+                            <i class="nav-icon fas fa-layer-group"></i>
+                            <p>
+                                Category
+                                <i class="fas fa-angle-left right"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="{{ route('categories.index') }}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>
+                                        Index
+                                    </p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ route('categories.create') }}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>
+                                        New Category
+                                    </p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
 
-                <li class="nav-item">
-                    <a href="seasons" class="nav-link">
-                        <i class="nav-icon fas fa-sun"></i>
-                        <p>
-                            Season
-                            <i class="fas fa-angle-left right"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="{{route('seasons.index')}}" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>
-                                    Index
-                                </p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{route('seasons.create')}}" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>
-                                    New Season
-                                </p>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
+                    <li class="nav-item">
+                        <a href="seasons" class="nav-link">
+                            <i class="nav-icon fas fa-sun"></i>
+                            <p>
+                                Season
+                                <i class="fas fa-angle-left right"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="{{ route('seasons.index') }}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>
+                                        Index
+                                    </p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ route('seasons.create') }}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>
+                                        New Season
+                                    </p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
 
-                {{-- <li class="nav-item">
+                    {{-- <li class="nav-item">
                     <a href="users" class="nav-link">
                         <i class="nav-icon fas fa-users"></i>
                         <p>
